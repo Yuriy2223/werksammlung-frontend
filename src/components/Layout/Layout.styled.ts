8bf8d8569b29481afb87b12195dffcfb266f9abf
Yuriy <>
2025-04-20 13:02:47 +0300

    update

--- src/components/Layout/Layout.styled.ts
+++ src/components/Layout/Layout.styled.ts
@@ -12,6 +12,7 @@ export const HeaderWrapper = styled.header`
   right: 0;
   z-index: 9;
   background-color: ${({ theme }) => theme.secondary};
+  box-shadow: 0 10px 20px -10px ${({ theme }) => theme.colorText}; /** */
 `;
 export const MainWrapper = styled.main`
   padding-top: 80px;
