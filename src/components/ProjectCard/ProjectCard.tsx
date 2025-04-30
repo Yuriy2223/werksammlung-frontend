829199b349329733bf8a0ea2eeb51d592a14691f
Yuriy <>
2025-05-01 00:03:57 +0300

    update project section

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Github, Globe } from "lucide-react";
-import { Project } from "./Projects";
+import { Project } from "../Projects/Projects";
 import {
   BackFace,
   CardWrapper,
@@ -23,6 +23,7 @@ interface ProjectCardProps {
 
 export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   const [isFlipped, setIsFlipped] = useState(false);
+  const cardRef = useRef<HTMLDivElement>(null);
 
   const handleClick = () => {
     if (window.matchMedia("(hover: none)").matches) {
@@ -30,8 +31,28 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
     }
   };
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent | TouchEvent) => {
+      if (
+        isFlipped &&
+        cardRef.current &&
+        !cardRef.current.contains(event.target as Node)
+      ) {
+        setIsFlipped(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    document.addEventListener("touchstart", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+      document.removeEventListener("touchstart", handleClickOutside);
+    };
+  }, [isFlipped]);
+
   return (
-    <CardWrapper onClick={handleClick}>
+    <CardWrapper onClick={handleClick} ref={cardRef}>
       <CardInner $isFlipped={isFlipped}>
         <FrontFace>
           <ProjectImg src={project.imgUrl} alt={project.title} />
@@ -50,19 +71,11 @@ export const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
             <ProjectDescription>{project.description}</ProjectDescription>
           </div>
           <WrapLinkProject>
-            <LinkProject
-              // href={project.githubUrl}
-              href="#"
-              target="_blank"
-            >
+            <LinkProject href={project.repUrl} target="_blank">
               GitHub
               <Github size={20} />
             </LinkProject>
-            <LinkProject
-              // href={project.demoUrl}
-              href="#"
-              target="_blank"
-            >
+            <LinkProject href={project.demoUrl} target="_blank">
               View project
               <Globe size={20} />
             </LinkProject>
