60ac36381d4a25e3deb5308284cdbcd326196525
Yuriy <>
2025-05-11 21:58:17 +0300

    Update Container.tsx

--- src/shared/Container.tsx
+++ src/shared/Container.tsx
@@ -4,7 +4,6 @@ import styled from "styled-components";
 const StyledContainer = styled.div`
   width: 100%;
   max-width: 1280px;
-  /* padding: 20px; */
   margin: 0 auto;
 
   @media (max-width: 374px) {
