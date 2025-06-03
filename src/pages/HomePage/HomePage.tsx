577225527a0546ef27fe8ef86d5f85833505c3c6
Yuriy <>
2025-06-03 13:30:58 +0300

    adjust

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,4 +1,4 @@
-import { About } from "../../components/About/About";
+// import { About } from "../../components/About/About";
 import { Skills } from "../../components/Skills/Skills";
 import { Projects } from "../../components/Projects/Projects";
 import { Contact } from "../../components/Contact/Contact";
@@ -6,7 +6,7 @@ import { Contact } from "../../components/Contact/Contact";
 const HomePage = () => {
   return (
     <>
-      <About />
+      {/* <About /> */}
       <Skills />
       <Projects />
       <Contact />
